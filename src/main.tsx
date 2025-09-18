4545048a71163b762f229555f2034be16fe57ee4
Scott Brady <scottbrady@Scotts-MacBook-Pro.local>
2025-09-17 21:44:50 -0600

    Added Authentication via Supabase and GitHub OAuth

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css'
 
 import { BrowserRouter as Router } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 
 const client = new QueryClient();
@@ -12,9 +13,11 @@ const client = new QueryClient();
 ReactDom.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={client}>
-            <Router>
-                <App />
-            </Router>
+            <AuthProvider>
+                <Router>
+                    <App />
+                </Router>
+            </AuthProvider>
         </QueryClientProvider>
     </React.StrictMode>,
 )
